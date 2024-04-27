4e304e291406dc7add4d190476ff3ae65472379b
anikafroehli <afroehli@student.ethz.ch>
2024-04-27 13:19:20 +0200

    Ahfang Code (date mal ihglese)

--- code.py
+++ code.py
@@ -4,7 +4,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('drug-use-health/data.csv', low_memory=False)
+data = pd.read_csv('drug-use-health/data.csv', low_memory=False, index_col=0)
 pd.set_option("display.max_columns", data.shape[1])
-print(data.shape)
-#alle columns mit fragen zu tobacco, marijuahna, alcohol raussuchen und neue csv datei machen
+#print(data.shape)
+data_copy = data.copy()
+columns = data.filter(regex = r'(IRTBL|CRAVE|CRAGP|INCTL|AVOID|FN|PLANE|RNOUT|REG|NMCHG|SVLHR|INFLU|NOINF|INCRS|SATIS|LOTMR|WAKE|ROUT|RGDY|RGWK|RGNM|LOTTM|GTOVR|LIMIT|KPLMT|NDMOR|LSEFX|CUT|WD2SX|WDSMT|EMOPB|EMCTD|PHLPB|PHCTD|LSACT|SERPB|PDANG|LAWTR|FMFPB|FMCTD|DRVIN|NDT|TX|HP|SN|YE|STND|PR|YFL|FRD|HLTIN|NDFLT|MED|MX|MM|WRK|MFU|RSK|DIFOBT|RSN|NUMDKPM|DIFGET|OFRSM|WILYR|AUAL|JOBNG)', axis=1).columns.tolist()
+data.drop(columns,axis=1, inplace=True)
+data = pd.concat([data,data_copy['MJONLYFLAG']], axis =1)
+#print(data.shape)
