5ffb06fd131bcb295fc0f8111c1021666c39cc64
anikafroehli <afroehli@student.ethz.ch>
2024-04-27 11:15:25 +0200

    Ahfang Code (date mal ihglese)

--- code.py
+++ code.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('drug-use-health/NSDUH-2019.tsv', sep='\t', low_memory=False)
+data = pd.read_csv('drug-use-health/data', low_memory=False)
 pd.set_option("display.max_columns", data.shape[1])
 #print(data.head(5))
