f2153d8490e2ed66aa7b323e0d5aa373c1f677a2
anikafroehli <afroehli@student.ethz.ch>
2024-06-17 18:27:22 +0200

    Feature selection mit bilder

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -3,5 +3,6 @@ from Feature_Selection_2 import *
 #Hyperparameter tuning:
 #print(everything(data,LogisticRegression(class_weight='balanced',max_iter=1000),parameter,1, 'hyperparameter'))
 #C=0.1 am besten
+#In console type in Logistic Regression
 everything(data,LogisticRegression(class_weight='balanced',max_iter=1000, C=0.1),parameter,1,'evaluation')
 
--- SVM.py
+++ SVM.py
@@ -3,4 +3,5 @@ parameter = {'C': [0.01], 'kernel': ['linear']}
 from Feature_Selection_2 import *
 #print(everything(data, SVC(probability=True, class_weight='balanced'), svm_parameters, 1, 'hyperparameter').head(1))
 #best: C = 0.01, linear, fold 4
+#In console type in SVM
 everything(data, SVC(probability=True, class_weight='balanced',C=0.01, kernel='linear'), parameter, 1, 'evaluation')
